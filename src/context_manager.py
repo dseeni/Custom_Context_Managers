428d071ccdb137e6fc8065031c929174fe748e0d
Deepak Seeni <dseeni@gmail.com>
2019-07-29 15:56:32 -0700

    Add data files, and move tests folder

---
+++ src/context_manager.py
@@ -0,0 +1,14 @@
+
+# with a context manager
+# open each file
+# read a few lines and csv snip
+# possible also sniff/infer data type
+# create a header extract
+# create a named tuple based on header
+# cast each row to the named tuple
+# view current files in context
+# if stop iteration, remove file from file que
+# store named tuple for each file
+# read the next row of each file
+# cast data types
+
