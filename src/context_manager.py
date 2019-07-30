172e67177de11f6f31607ec19fd82de050a48e97
Deepak Seeni <dseeni@gmail.com>
2019-07-29 17:56:54 -0700

    Move sniffer_extract and csv_parser into FilesIterator subclass

--- src/context_manager.py
+++ src/context_manager.py
@@ -17,39 +17,65 @@ from itertools import islice
 # cast data types
 
 
-class FileContextManger:
-    def __init__(self, file_names: tuple): # filenames to iterate over as tuple
+class FileContextManager:
+    def __init__(self, file_names: tuple, header=False): # filenames to iterate over as tuple
         self.file_names = file_names
+        self.header = header
 
     def __enter__(self):
-        return self
+        # enter context and return new instances of an iterator per file
+        return iter(self)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return False
 
+    def __iter__(self):
+        return self.FilesIterator(self)
+
+    class FilesIterator:
+        def __init__(self, file_context_manager):
+            self.file_context_manager = file_context_manager
+            self.file_names = self.file_context_manager.file_names
+            # print(self.file_names)
+
+        def __iter__(self):
+            # print('Calling PolyIterator instance __iter__')
+            return self
+
+        def __next__(self):
+            return self.csv_parser(self.sniffer_extract(), include_header=self.file_context_manager.header)
+                    # for row in f:
+                    #     print(row)
+            # # print('Calling __next__')
+            # if self._index >= self.poly_obj.__len__():
+            #     raise StopIteration
+            # else:
+            #     item = Poly(self.poly_obj.polygons[self._index][0], self.poly_obj.polygons[self._index][1])
+            #     self._index += 1
+            #     return item
+            # raise StopIteration
+
+        def sniffer_extract(self):
+            for file in self.file_names:
+                with open(file) as f:
+                    sample = f.read(2000)
+                    dialect = csv.Sniffer().sniff(sample)
+                return dialect
+
+        def csv_parser(self, sniffer_dialect,  include_header=False):
+            for file_name in self.file_names:
+                with open(file_name) as f:
+                    reader = csv.reader(f, sniffer_dialect)
+                    if not include_header:
+                        next(f)
+                    yield from reader
 
 
 
-def sniffer_extract(file_names):
-    for file in file_names:
-        with open(file) as f:
-            sample = f.read(2000)
-            dialect = csv.Sniffer().sniff(sample)
-        return dialect
-        # print(vars(dialect))
-        # print()
 
 
-def csv_parser(file_names, sniffer_dialect,  include_header=False):
-    for file_name in file_names:
-        with open(file_name) as f:
-            reader = csv.reader(f, sniffer_dialect)
-            if not include_header:
-                next(f)
-            yield from reader
 
-
-file_rows = csv_parser(fnames, sniffer_extract(fnames))
-print(list(islice(file_rows, 10)))
+# file_rows = csv_parser(fnames, sniffer_extract(fnames))
+# print(list(islice(file_rows, 10)))
 
 
--- src/playground.py
+++ src/playground.py
@@ -1,7 +1,7 @@
 from src.context_manager import *
 from src.constants import *
 import csv
-from itertools import islice
+from itertools import islice, chain
 from collections import namedtuple
 
 # file_reader = FileContextManger(fnames)
@@ -20,21 +20,25 @@ from collections import namedtuple
 #                 print(row)
 #             print()
 
-with open(fnames[0]) as f:
-    sample = f.read(2000)
-    dialect = csv.Sniffer().sniff(sample)
-print(vars(dialect))
+# with open(fnames[0]) as f:
+#     sample = f.read(2000)
+#     dialect = csv.Sniffer().sniff(sample)
+# print(vars(dialect))
+#
+# with open(fnames[0]) as f:
+#     _reader = csv.reader(f, dialect)
+#     for row in islice(_reader, 10):
+#         print(row)
+#
+#
+# def create_named_tuple_class(fname, class_name):
+#     fields = extract_field_names(fname)
+#     return namedtuple(class_name, fields)
+#
+# def extract_field_names(fname):
+#     reader = csv_parser(fname, include_header=True)
+#     return next(reader)
 
-with open(fnames[0]) as f:
-    _reader = csv.reader(f, dialect)
-    for row in islice(_reader, 10):
-        print(row)
+with FileContextManager(fnames, True) as f:
+    print(*islice(next(f), 100000), sep='\n')
 
-
-def create_named_tuple_class(fname, class_name):
-    fields = extract_field_names(fname)
-    return namedtuple(class_name, fields)
-
-def extract_field_names(fname):
-    reader = csv_parser(fname, include_header=True)
-    return next(reader)
