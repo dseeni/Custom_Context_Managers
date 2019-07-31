d8bb4373aaf4755965d136374e67b220ec7d3926
Deepak Seeni <dseeni@gmail.com>
2019-07-30 17:59:26 -0700

    Add minor name change to variable names in gen_file_context_manager()

--- src/gen_context_manager.py
+++ src/gen_context_manager.py
@@ -13,16 +13,16 @@ def get_dialect(file_obj):
 
 
 @contextmanager
-def gen_file_context_manager(file_name, parser, class_name):
+def gen_file_context_manager(file_name, single_parser, single_class_name):
     file_obj = open(file_name)
     reader = csv.reader(file_obj, get_dialect(file_obj))
 
     def cast_row():
         headers = map(lambda l: l.lower(), next(reader))
-        DataTuple = namedtuple(class_name, headers)
+        DataTuple = namedtuple(single_class_name, headers)
         for _ in reader:
             row = next(reader)
-            zipped = (fn(value) for value, fn in zip(row, parser))
+            zipped = (fn(value) for value, fn in zip(row, single_parser))
             parsed = DataTuple(*zipped)
             yield parsed
     try:
