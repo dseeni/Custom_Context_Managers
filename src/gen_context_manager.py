8b65aa4e994d81be6a8d4a2fe007803ddf4f65c9
Deepak Seeni <dseeni@gmail.com>
2019-07-30 17:57:49 -0700

    Add minor tweaks PEP 8

--- src/gen_context_manager.py
+++ src/gen_context_manager.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 from src.constants import *
 from itertools import islice
 
+
 def get_dialect(file_obj):
     sample = file_obj.read(2000)
     dialect = csv.Sniffer().sniff(sample)
@@ -19,7 +20,7 @@ def gen_file_context_manager(file_name, parser, class_name):
     def cast_row():
         headers = map(lambda l: l.lower(), next(reader))
         DataTuple = namedtuple(class_name, headers)
-        for row in reader:
+        for _ in reader:
             row = next(reader)
             zipped = (fn(value) for value, fn in zip(row, parser))
             parsed = DataTuple(*zipped)
@@ -34,9 +35,8 @@ def gen_file_context_manager(file_name, parser, class_name):
         # print('closing file')
         file_obj.close()
 
+
 for fname, parser, class_name in zip(fnames, parsers, class_names):
     with gen_file_context_manager(fname, parser, class_name) as f:
         print('39:', *list(islice(f, 100000)), sep='\n')
         # print(next(f))
-
-
