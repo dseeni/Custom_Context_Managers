365ad47ee52b427193a5dc803da5b3e8aaaabb24
Deepak Seeni <dseeni@gmail.com>
2019-07-30 18:44:41 -0700

    Add complete coverage tests for gen_context_manager.py

--- tests/test_gen_context_manager.py
+++ tests/test_gen_context_manager.py
@@ -11,22 +11,22 @@ def test_gen_context_manager_exhausts_file(create_gen_context_manager):
         pass
 
 
-# def test_gen_context_manager_return_values(create_gen_context_manager):
-#     with create_gen_context_manager as f:
-#         row = next(f)
-#         assert row.ssn == '100-53-9824'
-#         assert row.first_name == 'Sebastiano'
-#
-#
-# def test_gen_context_manager_headers(create_gen_context_manager):
-#     with create_gen_context_manager as f:
-#         assert 'ssn' in dir(f)
-#         assert 'first_name' in dir(f)
-#         assert 'last_name' in dir(f)
-#         assert 'language' in dir(f)
+def test_gen_context_manager_return_values(create_gen_context_manager):
+    with create_gen_context_manager as f:
+        row = next(f)
+        assert row.ssn == '100-53-9824'
+        assert row.first_name == 'Sebastiano'
 
 
-# def test_file_context_manager_stop_iteration(create_gen_context_manager):
-#     with create_gen_context_manager as f:
-#         assert len(list(islice(f, 1200))) == 1000
+def test_gen_context_manager_headers(create_gen_context_manager):
+    with create_gen_context_manager as f:
+        assert 'ssn' in dir(next(f))
+        assert 'first_name' in dir(next(f))
+        assert 'last_name' in dir(next(f))
+        assert 'language' in dir(next(f))
+
+
+def test_file_context_manager_stop_iteration(create_gen_context_manager):
+    with create_gen_context_manager as f:
+        assert len(list(islice(f, 1200))) == 1000
 
