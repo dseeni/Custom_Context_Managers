5d7ae36c0e953879b3d2f240436491ca940cb72b
Deepak Seeni <dseeni@gmail.com>
2019-08-05 13:45:21 -0700

    Merge getdiaclect() into main, refactor --> yield from generator function

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Deep Dive Part 2 Project 5
+
+### Python Context Managers: 
+
+- Manages multiple file resources    
+
+- Concatenates data rows from each and parses data types
+
+- Returns namedtuples for each row with respective field names 
+
+
--- src/gen_context_manager.py
+++ src/gen_context_manager.py
@@ -3,31 +3,18 @@ from collections import namedtuple
 from contextlib import contextmanager
 
 
-def get_dialect(file_obj):
-    sample = file_obj.read(2000)
-    dialect = csv.Sniffer().sniff(sample)
-    file_obj.seek(0)
-    return dialect
-
-
 @contextmanager
 def gen_file_context_manager(file_name, single_parser, single_class_name):
     file_obj = open(file_name)
-    reader = csv.reader(file_obj, get_dialect(file_obj))
-
-    def cast_row():
+    dialect = csv.Sniffer().sniff(file_obj.read(2000))
+    file_obj.seek(0)
+    try:
+        reader = csv.reader(file_obj, dialect)
         headers = map(lambda l: l.lower(), next(reader))
         DataTuple = namedtuple(single_class_name, headers)
-        while True:
-            try:
-                row = next(reader)
-                zipped = (fn(value) for value, fn in zip(row, single_parser))
-                parsed = DataTuple(*zipped)
-                yield parsed
-            except StopIteration:
-                break
-    try:
-        yield cast_row()
+        yield (DataTuple(*(fn(value) for value, fn
+                           in zip(row, single_parser))) for row in reader)
+
     finally:
         try:
             next(file_obj)
